63e85e1859db320502f5f3e3a04559c39876c32d
DjCeejay <lordceejay12@gmail.com>
2026-08-11 12:29:47 +0100

    desktop view of the menu

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -31,7 +31,7 @@
 
   <!-- Favicon -->
   <link rel="icon" href="{{ asset('assets/logo2.png') }}" type="image/png" />
-  <link rel="stylesheet" href="{{ asset('styles.css') }}?v=14" />
+  <link rel="stylesheet" href="{{ asset('styles.css') }}?v=16" />
 </head>
 <body>
   <header class="af-header">
@@ -490,6 +490,6 @@
     </div>
   </footer>
 
-  <script src="{{ asset('script.js') }}?v=35" defer></script>
+  <script src="{{ asset('script.js') }}?v=36" defer></script>
 </body>
 </html>
